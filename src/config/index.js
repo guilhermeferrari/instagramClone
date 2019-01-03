3aeea8abb72e7f3967268b2436c794471b8e60fb
Guilherme Ferrari <guilhermefbravin@gmail.com>
2019-01-03 00:10:55 -0200

    fim do primeiro video

---
+++ src/config/index.js
@@ -0,0 +1,10 @@
+export default {
+    images: {
+        heartIcon: require('../../assets/heart.png'),
+        bubbleIcon: require('../../assets/speech.png'),
+        arrowIcon: require('../../assets/arrow.png')
+    },
+    styleConstants: {
+        rowHeigth: 50
+    }
+};
